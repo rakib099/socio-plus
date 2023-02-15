dcb1e7f315262859b14e74e92cba0a2d90562cba
Rakib099 <rakibnabi97@gmail.com>
2023-02-15 23:30:33 +0600

    Homepage: Navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import {RouterProvider} from 'react-router-dom';
+import router from './routes/Routes/Routes';
 function App() {
   return (
-    <div className="App">
+    <div className="App max-w-[1440px] mx-auto">
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/layouts/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../pages/Home/Home/Home';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <>
+            <Navbar />
+            <Home />
+        </>
+    );
+};
+
+export default Main;
