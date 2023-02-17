5ebb2c991ae8da136cf33c514758f14479e09dcc
Rakib099 <rakibnabi97@gmail.com>
2023-02-17 11:27:34 +0600

    Like, unlike toggle added

--- src/pages/Shared/Post/Post.js
+++ src/pages/Shared/Post/Post.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FcLike, FcLikePlaceholder} from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 import img from '../../../assets/images/user-32.png';
 import msToDate from '../../../utils/msToDate';
 
 const Post = () => {
+    const [liked, setLiked] = useState(false);
     const date = msToDate(1676609263733);
+
+    const handleLike = () => {
+        setLiked(!liked);
+    }
 
     return (
         <div className="post border border-accent px-4 py-3 rounded-lg lg:w-1/2 mx-auto my-3">
@@ -23,7 +29,16 @@ const Post = () => {
             <figure className='w-96 mx-auto'>
                 <img src="https://static.fotor.com/app/features/img/aiimage/scenes/a%20realistic%20fox%20in%20the%20lake%20generated%20by%20ai%20image%20creator.png" alt="" className='rounded-lg w-full' />
             </figure>
-            <Link to='/post-details/01'><button className="btn btn-accent text-white mt-2">Details</button></Link>
+            <div className="action flex justify-between items-center">
+                {
+                    liked ?
+                    <FcLike onClick={handleLike} title='Unlike it!' className='text-3xl cursor-pointer'/>
+                    :
+                    <FcLikePlaceholder onClick={handleLike} title='Like it!' className='text-3xl cursor-pointer'/>
+                }
+                
+                <Link to='/post-details/01'><button className="btn btn-accent text-white mt-2">Details</button></Link>
+            </div>
         </div>
     );
 };
